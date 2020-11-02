11631e5dc9d3b7bdcdf5f2256507f5abcdb39744
Cameron Logie <cameronlogie24@gmail.com>
2020-11-02 16:12:55 +0000

    fizzbuzz done in javascript

--- FizzBuzz/spec/fizzBuzzSpec.js
+++ FizzBuzz/spec/fizzBuzzSpec.js
@@ -7,6 +7,47 @@ describe ('Fizzbuzz', function() {
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
+    it('not divisible by three', function() {
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.isDivisibleByThree(4)).toBe(false);
+    });
+
+    it('divisible by five', function() {
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+      expect(fizzbuzz.isDivisibleByFive(4)).toBe(false);
+    });
+
+    it('divisible by three and five', function() {
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.isDivisibleByThreeAndFive(15)).toBe(true);
+      expect(fizzbuzz.isDivisibleByThreeAndFive(4)).toBe(false);
+    });
+
+  });
+
+  describe('when playing says', function() {
+    it('Fizz when divisible by three and not five', function() {
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.say(3)).toBe('Fizz');
+    });
+
+    it('Buzz when divisible by five and not three', function() {
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.say(5)).toBe('Buzz');
+    });
+
+    it('FizzBuzz when divisible by both three and five', function() {
+      fizzbuzz = new Fizzbuzz(); 
+      expect(fizzbuzz.say(15)).toBe("FizzBuzz");
+    });
+
+    it('the number when not divisible by three or 5', function() {  
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.say(4)).toBe(4)
+
+    });
+
   });
 
 });
--- FizzBuzz/src/fizzBuzz.js
+++ FizzBuzz/src/fizzBuzz.js
@@ -1,7 +1,32 @@
 class Fizzbuzz {
 
+  say(number) {
+    if (this.isDivisibleByThreeAndFive(number)) {
+      return 'FizzBuzz'
+    }
+    if (this.isDivisibleByThree(number)) {
+      return 'Fizz'
+    }
+    if (this.isDivisibleByFive(number)) {
+      return 'Buzz'
+    }
+    return number;
+  }
+
   isDivisibleByThree(number) {
-    return number % 3 === 0;
+    return this._isDivisibleBy(number, 3)
   } 
+
+  isDivisibleByFive(number) {
+    return this._isDivisibleBy(number, 5)
+  }
+
+  isDivisibleByThreeAndFive(number) {
+    return this._isDivisibleBy(number, 15)
+  }
+
+  _isDivisibleBy(number, divisor) {
+    return number % divisor === 0
+  }
 
 }
