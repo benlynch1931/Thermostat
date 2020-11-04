eb0e8a88dd1f3cc3417e59883d9bc40d7bad97a1
Ben <ben.lynch1931@gmail.com>
2020-11-04 16:27:06 +0000

    continued turning specification into tests. i.e. powersavingmode

--- Thermostat/spec/ThermostatSpec.js
+++ Thermostat/spec/ThermostatSpec.js
@@ -9,13 +9,57 @@ describe('Thermostat',function () {
     expect(thermostat.temperature).toEqual(20)
   });
 
+  it('starts with minimum temperature of 10 degrees', function() {
+
+    expect(thermostat.minimumTemp).toEqual(10)
+  });
+
+  it('starts with power saving mode on', function() {
+    expect(thermostat.maximumTemp).toEqual(25);
+    expect(thermostat.isSavingMode).toEqual(true);
+  });
+
   describe('#up',function () {
 
-    it('increases the temperature by one each time',function () {
+    it('increases the temperature by one each time', function () {
       // thermostat.up();
 
       expect(thermostat.up()).toEqual(21)
     });
+  });
+
+  describe('#down',function () {
+
+    it('decreases the temperature by one each time', function () {
+      // thermostat.up();
+
+      expect(thermostat.down()).toEqual(19)
+    });
+
+    it('cannot decrease below 10 degrees', function() {
+      thermostat.temperature = 10;
+      expect(function() { thermostat.down() }).toThrowError("Minimum temperature reached!")
+    });
+  });
+
+  describe('#powerSavingModeOn', function() {
+
+    it('sets savingMode to true and max temp to 25', function() {
+      thermostat.powerSavingModeOn();
+      expect(thermostat.isSavingMode).toEqual(true);
+      expect(thermostat.maximumTemp).toEqual(25);
+    });
+
+  });
+
+  describe('#powerSavingModeOff', function() {
+
+    it('sets savingMode to false and max temp to 32', function() {
+      thermostat.powerSavingModeOff();
+      expect(thermostat.isSavingMode).toEqual(false);
+      expect(thermostat.maximumTemp).toEqual(32);
+    });
+
   });
 
 });
--- Thermostat/src/Thermostat.js
+++ Thermostat/src/Thermostat.js
@@ -1,10 +1,30 @@
 class Thermostat {
 
   constructor() {
+    this.minimumTemp = 10;
     this.temperature = 20;
+    this.isSavingMode = true;
+    this.maximumTemp = 25;
   }
 
   up() {
-    return this.temperature += 1
+    return this.temperature += 1;
+  }
+
+  down() {
+    if(this.temperature <= this.minimumTemp) {
+      throw new Error('Minimum temperature reached!');
+    }
+    return this.temperature -= 1;
+  }
+
+  powerSavingModeOn() {
+    this.isSavingMode = true;
+    this.maximumTemp = 25;
+  }
+
+  powerSavingModeOff() {
+    this.isSavingMode = false;
+    this.maximumTemp = 32;
   }
 }
